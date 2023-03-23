f8b2455353dd5aad6e00b3deea484cbcbfd93c03
Bruno La Porta <brunolpw@gmail.com>
2023-03-23 16:19:27 -0300

    ajustes

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ buttonSortEl.addEventListener('click', () => { sortList() });
 buttonResetEl.addEventListener('click', () => { resetList() });
 
 function createColorEl(color) {
-    return `<div class="color" style="background-color: ${color};"></div>`;
+    return `<div class="color" style="background-color: ${color};">
+        <abbr title="${color}">X</abbr>
+    </div>`;
 }
 
 function setColorListEl(colors) {
@@ -36,14 +38,16 @@ function generateColors() {
 
 function sortList() {
     const sortedList = colorsList.map(color => {
-        color = color.includes('#') 
-            ? hexToRgb(color) 
+        color = color.includes('#')
+            ? hexToRgb(color)
             : formatRgb(color);
 
         return rgbToHsl(...color);
     })
-    .sort((a, b) => a[0] - b[0] > 0 ? 1 : -1)
-    .map(hslColor => `hsl(${hslColor[0]}, ${hslColor[1]}%, ${hslColor[2]}%)`);
+        .sort((a, b) => a[0] - b[0] > 0 ? 1 : -1) // by hue
+        //.sort((a, b) => a[1] - b[1] > 0 ? 1 : -1)  // by saturation
+        //.sort((a, b) => a[2] - b[2] > 0 ? 1 : -1) // by brightness
+        .map(hslColor => `hsl(${hslColor[0]}, ${hslColor[1]}%, ${hslColor[2]}%)`);
 
     setColorListEl(sortedList);
 }
@@ -52,7 +56,7 @@ function resetList() {
     colorsList = [
         "#fbb735", "#e98931", "#eb403b", "#b32E37", "#6c2a6a",
         "#5c4399", "#274389", "#1f5ea8", "#227FB0", "#2ab0c5",
-        "#39c0b3"
+        "#39c0b3", "#efa94a", "#82898f", "#3f888f", "#cb3234",
 	].sort();
     
     setColorListEl(colorsList);
